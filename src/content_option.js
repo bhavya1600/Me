f0938908cb8e31207481057279ba4c3dbb059bad
Bhavya Chhabra <bhavya1600@gmail.com>
2024-12-01 02:00:04 -0500

    Projects

--- src/content_option.js
+++ src/content_option.js
@@ -17,12 +17,12 @@ const introdata = {
 
 const dataabout = {
     title: "a bit about my self",
-    aboutme: "Hi, I'm Bhavya, an aspiring data scientist based in Canada. My work bridges the gap between technology and practical applications, focusing on using advanced analytics and machine learning to solve real-world problems. With experience ranging from developing AI models at \
+    aboutme: "hi, I'm Bhavya, an aspiring data scientist based in Canada. My work bridges the gap between technology and practical applications, focusing on using advanced analytics and machine learning to solve real-world problems. With experience ranging from developing AI models at \
     London Hydro to innovative projects in anomaly detection and infrastructure inspection, my career is grounded in applying Python, AI, and cloud technologies to enhance efficiency and decision-making. Holding a Master’s in Computer Science with a specialization in AI, I am committed to \
     lifelong learning and applying my skills to make a tangible impact."
 };
 const worktimeline = [{
-        jobtitle: "Business Systems Analyst",
+        jobtitle: "Data Science and DevOps Engineer",
         where: "London Hydro",
         date: "May 2023-Present",
     },
@@ -64,13 +64,26 @@ const skills = [{
     },
 ];
 
-const services = [{
-        title: "Autonomous Drone for Infrastructural Inspection and Fault Detection ",
-        description: "Automated an open-source drone (CrazyFlie) to fly to a specified portion of a building and survey for detecting faults or cracks using object detection and instance segmentation with YOLOv5 (mAP score: 88.5%, Seg Loss: 0.8%). Annotated and collected data manually. Deployed using Raspberry PI",
+const services = [
+    {
+        title: "Electric Vehicle Detection using Hourly Energy Usage Data - London Hydro | Python, TensorFlow Oct 2024",
+        description: "Developed a Deep Convolutional LSTM model to accurately identify and segment over 30,000 electric vehicle customers based on hourly electricity usage data. This segmentation enabled targeted marketing for the new Ultra Low Overnight price plan, increasing customer engagement and adoption rates.",
+    },
+    {
+        title: "Billing Forecast System - London Hydro | Python, Sklearn, MySQL Jan 2024",
+        description: "Utilized XGBoost Decision Trees to refine the existing non-ML based billing forecast system, decreasing percentage error by 60%. Improved forecast accuracy and customer satisfaction for over 100,000 users.",
+    },
+    {
+        title: "Directory Services Web Development - London Hydro | AWS, JavaScript, HTML, CSS Jun 2023",
+        description: "Designed and implemented the Directory Services section for greenbuttonalliance.org, overcoming Novi AMS limitations by creating custom scripts hosted on AWS and embedding them via iframes. Built three distinct directories—Utilities, Third Party App Providers, and Platform Providers—by fetching data through API calls, applying business logic for content segregation, and dynamically displaying searchable, filterable, and paginated cards. Implemented dynamic search tools and filters, enabling efficient partner discovery and seamless collaboration between data custodians, third parties, and service providers.",
+    },
+    {
+        title: "Autonomous Drone for Infrastructural Inspection and Fault Detection",
+        description: "Automated an open-source drone (CrazyFlie) to fly to a specified portion of a building and survey for detecting faults or cracks using object detection and instance segmentation with YOLOv5 (mAP score: 88.5%, Seg Loss: 0.8%). Annotated and collected data manually. Deployed using Raspberry Pi.",
     },
     {
         title: "Gastrointestinal System Anomaly Detection",
-        description: "Teamed up to build and train a Vision Transformer model for 12-class anomaly detection in the Gastrointestinal (GI) system using AWS. Achieved 93% accuracy and sensitivity, significantly reducing diagnosis time.",
+        description: "Teamed up to build and train a Vision Transformer model for 12-class anomaly detection in the gastrointestinal system using AWS. Achieved 93% accuracy and sensitivity, significantly reducing diagnosis time.",
     },
     {
         title: "Advanced Text Classification via LSTM Networks with Hierarchical Attention Mechanism",
@@ -78,21 +91,22 @@ const services = [{
     },
     {
         title: "Melanoma Skin Cancer Classification using a Custom Integrated Model",
-        description: "Conceptualized and innovated a Custom Integrated Computer Vision model by combining a CNN with a tabular model using PyTorch, FastAI framework for improving accuracy in Melanoma classification tasks and utilized the model using StreamLit",
+        description: "Conceptualized and innovated a custom integrated computer vision model by combining a CNN with a tabular model using PyTorch and the FastAI framework to improve accuracy in melanoma classification tasks and deployed the model using Streamlit.",
     },
     {
         title: "Stellar Object Classification - ML Comparative Analysis",
-        description: "Experimented and compared various ML Algorithms to determine Stellar object classes.",
+        description: "Experimented with and compared various ML algorithms to determine stellar object classes.",
     },
     {
         title: "Forecasting USA Elections using Twitter Data",
-        description: "Predicted Joe Biden's victory in USA Presidential elections by large scale sentiment analysis using tweets spanning over 6 months.",
+        description: "Predicted Joe Biden's victory in the USA presidential elections through large-scale sentiment analysis of tweets spanning over six months.",
     },
     {
         title: "Facial Emotion Classifier",
-        description: "Developed a multi-class image classifier to detect 6 emotion classes with 85% Accuracy",
+        description: "Developed a multi-class image classifier to detect six emotion classes with 85% accuracy.",
     },
 ];
+
 
 const dataportfolio = [{
         img: "https://github.com/bhavya1600/Me/blob/main/assets/img/drone.png?raw=true",
